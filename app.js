4bd99bdf09665c5e1ba2837ea7b4a9810668528e
Mykola Hubytskyi <hubytskyi@gmail.com>
2023-10-12 21:47:48 +0300

    add webinar 1

---
+++ app.js
@@ -0,0 +1,219 @@
+"use strict";
+
+// var one;
+
+// console.log(one);
+// console.log(two);
+
+// one = 1;
+// let two = 2;
+
+// Одностроковий коментар
+
+/* Багатостроковий
+    коментар
+    ...
+*/
+
+// TODO: оновити отримання даних
+
+// let message2;
+// message2 = "привіт";
+
+// let message2 = "hello";
+// message2 = 42;
+
+// const name = "Андрій";
+// name = "Олег";
+
+// const return = 'some tex';
+
+// 'admin'
+
+// const USER_ADMIN = 'ADMIN';
+
+// const userAdmin
+
+// console.log(1n);
+// console.log(12);
+
+// console.log("Користувач 1 ${message2}");
+// console.log("Користувач 2");
+// console.log(`Користувач 3`);
+
+// const isVisible = true;
+// const isVisible2 = false;
+
+// const user = {
+//   name: "Петро",
+//   age: 42,
+// };
+
+// const array = [];
+// function sum() {}
+
+// let user2 = null;
+// let user3;
+
+// console.log(user2);
+// console.log(user3);
+
+// console.log(12 % 5);
+// console.log(15 % 5);
+
+// console.log(10 < 10);
+// console.log(10 != 10);
+
+// console.log("10s" === 10);
+
+// console.log(true && true && true && false);
+// console.log(true || false || false);
+// console.log(!true);
+// console.log(!false);
+
+// 0, null, undefined, false, '', NaN
+// console.log(!!0);
+// console.log(!!null);
+// console.log(!!undefined);
+// console.log(!!false);
+// console.log(!!"");
+// console.log(!!NaN);
+
+// Boolean(0) === !!0
+
+// let i = 3;
+
+// while (i < 3) {
+//   console.log(i);
+//   i++; // i = i + 1;
+// }
+
+// let y = 3;
+
+// do {
+//   console.log(y);
+//   y++; // i = i + 1;
+// } while (y < 3);
+
+// for (let i = 0; i < 3; i++) {
+//   console.log(i);
+// }
+
+// let sum = 0;
+
+// while (true) {
+//   const value = prompt("Введіть число", ""); // 10, '10' + '10', '1010';
+
+//   if (!value) break;
+
+//   sum += value; // sum = sum + value;
+// }
+
+// alert(sum);
+
+// for (let i = 0; i < 10; i++) {
+//   console.log("i: ", i);
+//   if (i % 2 === 0) continue;
+
+//   console.log("i after condition: ", i);
+// }
+
+// const year = prompt("Коли з'явився JS?", "");
+
+// console.log(year);
+
+// if (year < 1995) {
+//   console.log("Зарано");
+// } else if (year > 1995) {
+//   console.log("Запізно");
+// } else {
+//   console.log("Вірно");
+// }
+
+// const age = prompt("Вкажіть свій вік", "18"); // другий аргумент, як значення за замовчуванням
+
+// const isAllowed = age >= 18 ? true : false;
+// const isAllowed3 = age >= 18;
+
+// console.log("isAllowed: ", isAllowed);
+// console.log("isAllowed3: ", isAllowed3);
+
+// let isAllowed2;
+
+// if (age >= 18) {
+//     isAllowed2 = true
+// } else {
+//     isAllowed2 = false;
+// }
+
+// const user = {
+//   name: "Петро",
+//   age: 42,
+// };
+
+// if (user.name === "Петро") {
+//   // ...
+// } else if (user.name === "Петро" && user.age > 18) {
+//   // ...
+// }
+
+// const sum = 45;
+
+// switch (sum) {
+//   case 42:
+//     console.log("Вірно");
+//     break;
+//   case 45:
+//     console.log("Багато");
+//     break;
+//   case 20:
+//     console.log("Мало");
+//     break;
+//   default:
+//     console.log("Невідомо");
+// }
+
+// if (sum === 42) {
+//   console.log("Вірно");
+// } else if (sum === 45) {
+//   console.log("Багато");
+// }
+
+// const user1 = {
+//   name: "Петро",
+//   age: 42,
+// };
+
+// const user2 = { // 45gw5gs5vw5vtw
+//   name: "Петро",
+//   age: 42,
+// };
+
+// let a = 1;
+// let b = 1;
+
+// let c = a; // c === 1; c === a
+// a = 2;
+// const user3 = user1;
+// user1.name = "Максим";
+// user3.name === user1.name;
+
+// console.log(user3 === user1);
+
+// console.log(user1 === user2);
+
+// for (let i = 0; i < 3; i++) {
+//   var one = 1;
+//   let two = 2;
+// }
+
+// console.log(one);
+// console.log(two);
+
+// var one = 1;
+// var one = 2;
+
+// one = 3;
+
+// let two = 2;
+// two = 2;
