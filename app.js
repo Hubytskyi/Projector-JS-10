76d1cb32adce3d98dd56e2110c65c9be4a022e6a
Mykola Hubytskyi <hubytskyi@gmail.com>
2023-11-03 08:32:19 +0200

    add set and map

--- app.js
+++ app.js
@@ -1,205 +1,133 @@
-"use strict";
+'use strict';
 
-// const stack = ["HTML", "CSS", "JS"];
-// console.log(stack[0]);
-// console.log(stack);
+// const arr = ['orange', 'pineapple', 'orange'];
+// const fruits = new Set(arr);
 
-// stack[1] = "SCSS";
-// console.log(stack);
-// stack[3] = "React";
-// console.log(stack);
-// stack[10] = "React";
-// console.log(stack[4]);
-// console.log(stack);
-// console.log(stack.length);
+// console.log(fruits)
 
-// console.log(stack[stack.length - 1]);
-// console.log(stack.at(-1));
-// console.log(typeof stack); {}
+// const uniqueIds = new Set([1,2,3,4,4,5,5,6,7]);
 
-// console.log(Array.isArray(stack)); // true
-// console.log(Array.isArray("stack")); // false
+// console.log('uniqueIds: ', uniqueIds);
+// console.log('uniqueIds size: ', uniqueIds.size);
 
-// console.log(stack);
-// const result1 = stack.push("React");
-// const result2 = stack.unshift("Angular");
-// const result3 = stack.pop();
-// const result4 = stack.shift();
-// console.log(stack);
-// console.log(stack.length);
-// console.log(result4);
+// uniqueIds.add(8)
 
-// for (let i = 0; i < stack.length; i++) {
-//   console.log(stack[i]);
+// console.log('uniqueIds: ', uniqueIds);
+// console.log('uniqueIds size: ', uniqueIds.size);
+
+// console.log('uniqueIds has: ', uniqueIds.has(9))
+// console.log('uniqueIds has: ', uniqueIds.has(8))
+
+// uniqueIds.delete(8);
+
+// console.log('uniqueIds: ', uniqueIds);
+// console.log('uniqueIds size: ', uniqueIds.size);
+
+// uniqueIds.clear();
+// console.log('uniqueIds: ', uniqueIds);
+
+
+// fruits.forEach((fruit, fruitAgain, coll) => {
+//     console.log('set: ', fruit, fruitAgain, coll)
+// })
+
+// arr.forEach((fruit, fruitAgain, array) => {
+//     console.log('arr: ', fruit, fruitAgain, array)
+// })
+
+// const arr = ['orange', 'pineapple', 'orange'];
+// const fruits = new Set(arr);
+
+// for(let value of fruits) {
+//     console.log(value)
 // }
 
-// stack.forEach((el, i, arr) => { // params: 1 - element, 2 - index, 3 - current array
-//   console.log(i, el, arr);
-// });
+// const cheapShirt = { size: 'L', color: 'white' };
+// const fancyShirt = { size: 'L', color: 'white' };
 
-// const stack = ["HTML", "CSS", "JS"];
-// const animals = ["Dogs", "Cats", "Rabbit"];
-// const nums = [1, "2", 5, 13, "42", 99, "8"]; // 1, 13, 2, 42, 5, 8, 99
+// console.log(cheapShirt === fancyShirt);
+// console.log(cheapShirt === cheapShirt);
 
-// stack.splice(1, 0, "SCSS", "SASS", "VUE"); // start - 1 (CSS), deleteCount - 1 (CSS), items - SCSS
-// console.log(stack);
+// const set = new Set([cheapShirt, cheapShirt, fancyShirt]);
+// const set2 = new Set([{}, {}, {}]);
+// console.log(set2);
 
-// const result = stack.slice(1, 2);
+// const set = new Set([1,2,3,4,4,5,5,6,7])
+// const nums = [1,2,3,4,4,5,5,6,7];
+// const uniqueIds = [...new Set([1,2,3,4,4,5,5,6,7])];
+// console.log(set)
+// console.log(uniqueIds)
 
-// console.log(stack);
-// const result = stack.concat(animals, [1, 2, 3]);
-// const result2 = [].concat(animals, [1, 2, 3]);
-// console.log(stack);
-// console.log(result);
+// [1,2,3,4,4,5,5,6,7] + ... = 1,2,3,4,4,5,5,6,7
+// [1,2,3, [1,2,3]] + ... = 1,2,3,[1,2,3]
+// const deepArray = [1,2,3,4,[1,2,3,5, [3454564]]];
+// console.log(deepArray.flat(Infinity));
 
-// const arr = [
-//   {
-//     id: 1,
-//     userName: "John",
-//   },
-//   {
-//     id: 2,
-//     userName: "Qwe",
-//   },
-// ];
+const user = {
+    id: 1,
+    name: "John",
+    role: "Software Engineer",
+    age: 10,
+    true: 'asdsa',
+};
 
-// stack.sort();
-// nums.sort((numA, numB) => {
-//   return numB - numA;
-// });
+const userMap = new Map([
+    ['id', 1],
+    ['name', 'John'],
+    ['role', 'Software Engineer'],
+    ['age', 10],
+    ['Name Book', "Author Book"]
+]);
 
-// nums.sort((objA, objB) => {
-//   return objA.id - objB.id;
-// });
+// console.log(user.name)
 
-// console.log(stack);
-// console.log(nums);
-// const arr = [];
-
-// animals.forEach((animal) => {
-//   arr.push(animal.toUpperCase());
-// });
-// const animals = ["Dogs", "Cats", "Rabbit"];
-
-// const result2 = animals.map((animal) => {
-//   if (animal.length > 5) {
-//     return animal.toUpperCase();
-//   }
-
-//   return animal;
-// }); // animals.length === result2.length ALWAYS!!!
-// const arr = [...animals];
-// console.log(animals);
-// console.log(...animals);
-// console.log(arr);
-// const arr = [].concat(animals);
-// const res = arr.reverse();
-// console.log(animals);
-// console.log(res);
-
-// const str = "Hello world!";
-// const splitedStr = str.split(" ");
-// const splitedStr = str.split(" ")[0];
-
-// const updatedArr = splitedStr.map((word) => {
-//   return `${word}-`;
-// });
-
-// const joinedArr = updatedArr.join(" --- ");
-
-// console.log(joinedArr);
-
-// const animals = ["Dogs", "Cats", "Rabbit"];
-// console.log(animals.indexOf("Dogs"));
-
-// if (animals.indexOf("Dogs", 1)) {
-//   console.log("Dogs");
-// } else {
-//   console.log("Empty");
+// function functionLikeKey() {
+//     return 123
 // }
 
-// if (animals.includes("Dogs", 1)) {
-//   console.log("Dogs");
-// } else {
-//   console.log("Empty");
+// userMap.set(true, 'test string')
+// userMap.set(functionLikeKey, 'value of function')
+// console.log(userMap)
+
+// console.log(userMap.get('Name Book'))
+
+// for(let key of Object.keys(user)) {
+//     console.log('user: ', key)
 // }
 
-// const result = animals.find((animal) => {
-//   if (animal === "Cats") {
-//     return animal;
-//   }
-// });
-
-// const result3 = animals.find((animal) => {
-//   if (animal === "Cats") {
-//     return true;
-//   }
-// });
-
-// const result2 = animals.find((animal) => animal.length > 5);
-
-const animals = ["Dogs", "Dogs", "Cats", "Rabbit"];
-// const result = animals.filter((animal) => animal === "Dogs");
-
-// const result2 = animals.filter((animal) => {
-//   if (animal.length === 4) {
-//     return animal;
-//   }
-// });
-
-// const result3 = animals.filter((animal) => {
-//   if (animal.length === 4) {
-//     return true;
-//   }
-// });
-
-// const filteredArr = [];
-
-// for (let i = 0; i < animals.length; i++) {
-//   if (!filteredArr.includes(animals[i]) && animals[i] === "Dogs") {
-//     filteredArr.push(animals[i])
-//   }
+// for(let key of userMap.keys()) {
+//     console.log('userMap key: ', userMap.get(key))
 // }
-// console.log(result);
 
-// const arr = [12, 3, 4, 5, [1, 2, 3, [3, [[[5]]]]]];
-// console.log(arr.flat(Infinity));
+// for(let value of userMap.values()) {
+//     console.log('userMap value: ', value)
+// }
 
-// const nums = [1, 2, 3, 4, 5, 6, 7, 8];
+// for(let el of userMap) {
+//     console.log('userMap el: ', el)
+// }
 
-// const result = nums.some((num) => {
-//   return num > 8;
-// });
+// userMap.forEach((value, key, map) => {
+//     console.log(value, key, map)
+// })
 
-// const result2 = nums.every((num) => {
-//   return num > 0;
-// });
+// let john = {name: 'John'};
+// const arr = [john];
 
-// console.log(result2);
+// console.log(john)
+// console.log(arr)
 
-// const user = {
-//   name: "Марія",
-//   age: 20,
-//   id: 1,
-// };
+// const weakMap = new WeakMap();
+// // weakMap.set(john, '...')
 
-// const users = [user];
+// john = null;
 
-// const nums = ['Губицький Микола']; // 'Губицький Микола' split(' ') // ['Губицький', 'Микола']
-// (el, index, array) => {}
-// (acc, cur, index, array) => {}
-const result = nums.reduce((acc, cur, index, arr) => {
-  return acc + cur;
-}); // 0 - початкове значення акамулятора (acc)
+// let sum = 0;
 
-console.log(result);
+// for(let i = 0; i < 100000000; i++) {
+//     sum+=i;
+// }
 
-// 1
-// acc = 0
-// cur = 1
-// return acc(0) + cur(1) = 1
-
-// 2
-// acc = 1
-// cur = 2
-// return acc(1) + cur(2) = 3
+// console.log(john)
+// // console.log(weakMap)
+// console.log(arr)
