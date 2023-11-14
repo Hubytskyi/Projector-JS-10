55c36bfda3501a238049bcc416da9c4bda497305
Mykola Hubytskyi <hubytskyi@gmail.com>
2023-11-14 23:06:58 +0200

    add advanced functions

--- app.js
+++ app.js
@@ -1,200 +1,86 @@
 "use strict";
 
-// const obj1 = {
+// function sayHello() {
+//   alert("Hello");
+
+//   setTimeout(sayHello, 2000);
+// }
+
+// const id = setTimeout(sayHello, 2000);
+
+// const id2 = setTimeout(sayHello, 3000);
+
+// clearTimeout(id2);
+
+// const id = setInterval(sayHello, 1000);
+
+// clearInterval(id);
+
+// setInterval(sayHello, 1000); // alert
+
+// function getName(role, test) {
+//   console.log(`My name is ${this.name}, ${role}, ${test}`);
+//   return 1;
+// }
+
+// const name = "Jane";
+
+// const user1 = {
+//   id: 1,
 //   name: "John",
-// };
-
-// const obj2 = {
-//   name2: "Jane",
-// };
-
-// const num1 = 1;
-// const num2 = 2;
-
-// const result = Math.max(num1, num1, num1, num1, num1, num2);
-// console.log(result);
-
-// const math = {
-//   max: (...nums) => {
-//     console.log(nums);
+//   age: 42,
+//   sayHello: function () {
+//     console.log(`My name is ${user1.name}`);
+//   },
+//   getName: getName,
+//   roles: ["admin", "user"],
+//   showRoles() {
+//     this.roles.forEach((role) => {
+//       console.log("this: ", this);
+//       console.log(`My name is ${this.name}, my role - ${role}`);
+//     });
 //   },
 // };
 
-// math.max(num1, num1, num1, num1, num1, num2);
+// setTimeout(user1.getName.bind(user1), 1000);
+// const newGetName = user1.getName.bind(user1);
 
-// const obj3 = Object.assign(obj1, obj2, ...);
-// console.log(obj3);
+// якщо ми передаємо функцію як аргумент або записуємо її в змінну
+// і якщо функція не викликається - втрачається контекст
 
-// function sumAll(...args) {
-//   // args = [1, 4, 6, 6]
-//   let sum = 0;
+// newGetName();
 
-//   for (let arg of args) {
-//     sum += arg;
-//   }
-
-//   return sum;
-// }
-
-// console.log(sumAll(1));
-// console.log(sumAll(1, 2, 3));
-// console.log(sumAll(1, 4, 6, 6));
-
-// function myBio(firstName, lastName, ...other) {
-//   console.log(arguments[0]); // не використовувати!
-//   return other; // "Developer", "JS"
-// }
-
-// console.log(myBio("John", "Jane", "Developer", "JS"));
-// myBio("John", "Jane", "Developer", "JS");
-
-// const arr = [1, 2, 3, 5];
-// const res = Math.max(...arr); // arr => 1,2,3,5
-// console.log(res);
-
-// const math = {
-//   max: (...nums) => {
-//     console.log(nums);
+// const user2 = {
+//   id: 2,
+//   atest: "Jane",
+//   age: 22,
+//   sayHello: () => {
+//     console.log(this);
+//     console.log(`My name is ${this.age}`);
 //   },
+//   getName, // getName: getName === getName
 // };
 
-// math.max(...arr);
+// user2.sayHello();
 
-// function myBio(firstName, lastName, position, stack) {
-//   return `${firstName}, ${lastName}, ${position}, ${stack}`; // "Developer", "JS"
-// }
-
-// const arr = ["John", "Jane", "Developer", "JS"];
-// console.log(myBio(...arr)); // "John", "Jane", "Developer", "JS"
-
-// Lex Env
-
-// globalLexEnv = {
-//     environmentRecord: {
-//         getUser: посилання на функцію
-//         getUser: посилання на функцію
-//         getUser: посилання на функцію
-//         getUser: посилання на функцію
-//         getUser: посилання на функцію
-//         getUser: посилання на функцію
-//         getUser: посилання на функцію
-//         getUser: посилання на функцію
-//         getUser: посилання на функцію
-//     },
-//     outer: null
-// }
-
-// function getUser(name) {
-//  getUserLexEnv = {
-//     environmentRecord: {
-//         name: "John",
-//         age: 42
-//     },
-//     outer: посилання на глобальний об'єкт
-// }
-//   const age = 42;
-
-//   return `${name}, ${age}`;
-// }
-
-// getUser("John");
-
-// const a = "global";
+// user1.showRoles();
+// user2.sayHello();
+// console.log(this);
+// const arr = ["admin", "test"]
+// const newGetName = user1.getName.bind(user1, "admin", "test"); // кількість параметрів
+// const res = user1.getName.call(user1, "admin", "test"); // кількість параметрів
+// user1.getName.apply(user1, arr); // масив параметрів
+// console.log(newGetName());
+// bind - не викликає функцію, а тільки змінює контекст
+// call, apply - викликають функцію одразу
 
 // function outer() {
-//   const b = "outer";
-
+//   console.log(this);
 //   function inner() {
-//     const c = "inner";
-
-//     console.log(c);
-//     console.log(b);
-//     console.log(a);
+//     console.log(this);
 //   }
-
-//   console.log(a);
-//   console.log(b);
 
 //   inner();
 // }
 
 // outer();
-// console.log(a);
-
-// шукаємо name
-// const name = "John";
-
-// function getPersonName() {
-//   // шукаємо name
-//   const name = "Jane";
-
-//   return function displayName() {
-//     // шукаємо name
-//     console.log(name);
-//   };
-// }
-
-// const person = getPersonName();
-// person();
-// console.log(person);
-
-//  getCounterLexEnv = {
-//     environmentRecord: {
-//         count: "John",
-//         anom: посилання на функцію
-//     },
-//     outer: посилання на глобальний об'єкт
-// }
-
-//  getCounterLexEnv = {
-//     environmentRecord: {
-//
-//     },
-//     outer: посилання на батьківську функцію
-// }
-
-// function getCounter() {
-//   let count = 0;
-
-//   return function () {
-//     return count++;
-//   };
-// }
-
-// const counter = getCounter();
-// const counter1 = getCounter();
-// console.log(counter);
-
-// console.log(counter());
-// console.log(counter());
-// console.log(counter());
-// console.log(counter1());
-// console.log(counter1());
-// console.log(counter1());
-
-// let count = 0;
-
-// function counter() {
-//   return count++;
-// }
-
-// console.log(counter());
-// console.log(counter());
-// console.log(counter());
-
-// function getCounter() {
-//   let count = 0;
-
-//   return count++;
-// }
-// console.log(getCounter());
-// console.log(getCounter());
-// console.log(getCounter());
-
-function aFunc() {}
-
-const aSomeNum = 1;
-// var aNextNum = 2;
-
-// console.log(window);
-console.log(globalThis);
