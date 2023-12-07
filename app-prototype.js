e640192358c8f74046f6c04b96cfc800330e6c9e
Mykola Hubytskyi <hubytskyi@gmail.com>
2023-12-07 22:51:46 +0200

    add booklist workshop

--- app-prototype.js
+++ app-prototype.js
@@ -1,58 +1,52 @@
-"use strict";
-
 const form = document.querySelector(".book-form");
 const bookList = document.querySelector(".book-list");
 
-class Book {
-  constructor(title, author, isbn) {
-    this.title = title;
-    this.author = author;
-    this.isbn = isbn;
-  }
+function Book(title, author, isbn) {
+  this.title = title;
+  this.author = author;
+  this.isbn = isbn;
 }
 
-class UI {
-  constructor(form) {
-    this.form = form;
-    this.tableBody = document.querySelector(".book-list");
-  }
+function UI(form) {
+  this.form = form;
+  this.tableBody = document.querySelector(".book-list");
+}
 
-  addBook(book) {
-    const row = document.createElement("tr");
+UI.prototype.addBook = function (book) {
+  const row = document.createElement("tr");
 
-    row.innerHTML = `
+  row.innerHTML = `
       <td>${book.title}</td>
       <td>${book.author}</td>
       <td>${book.isbn}</td>
       <td><button class="delete-book">x</button></td>
     `;
 
-    this.tableBody.appendChild(row);
+  this.tableBody.appendChild(row);
+};
+
+UI.prototype.removeBook = function (target) {
+  if (target.classList.contains("delete-book")) {
+    target.closest("tr").remove();
+    this.showAlert("Ви успішно видалили книгу!", "success");
   }
+};
 
-  removeBook(target) {
-    if (target.classList.contains("delete-book")) {
-      target.closest("tr").remove();
-      this.showAlert("Ви успішно видалили книгу!", "success");
-    }
-  }
+UI.prototype.showAlert = function (message, className) {
+  const div = document.createElement("div");
+  div.classList = className;
+  div.innerHTML = message;
 
-  showAlert(message, className) {
-    const div = document.createElement("div");
-    div.classList = className;
-    div.innerHTML = message;
+  this.form.before(div);
 
-    this.form.before(div);
+  setTimeout(() => {
+    div.remove();
+  }, 3000);
+};
 
-    setTimeout(() => {
-      div.remove();
-    }, 3000);
-  }
-
-  resetForm() {
-    this.form.reset();
-  }
-}
+UI.prototype.resetForm = function (form) {
+  this.form.reset();
+};
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
@@ -62,7 +56,7 @@ form.addEventListener("submit", (event) => {
   const isbnValue = event.target.isbn.value.trim();
 
   const ui = new UI(form);
-
+  console.log("ui: ", ui);
   if ([titleValue, authorValue, isbnValue].includes("")) {
     ui.showAlert("Необхідно заповнити всі поля", "error");
   } else {
