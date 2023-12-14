3d74cdfeb341af7dac2c2e17739389cc68653f59
Mykola Hubytskyi <hubytskyi@gmail.com>
2023-12-14 20:59:31 +0200

    add async 2

--- app.js
+++ app.js
@@ -1,82 +1,137 @@
 "use strict";
 
-// function one() {
-//   let sum = 0;
-//   for (let i = 0; i < 1000000000; i++) {
-//     sum += i;
+// let isFormLoading = false;
+
+// const promise = new Promise((resolveFn, rejectFn) => {
+//   // ...
+
+//   const result = {
+//     data: [{ userId: 1, name: "John", age: 42 }],
+//     ok: true,
+//   };
+
+//   if (result.ok) {
+//     resolveFn(result.data);
+//     return;
 //   }
 
-//   two();
-//   console.log(sum);
-//   return sum;
+//   rejectFn(new Error("щось трапилось"));
+// });
+
+// promise стан = fulfilled --> then (приймає те що ми передали в resolveFn)
+// promise стан = rejected --> catch (приймає помилку яку ми передали в rejectFn)
+// promise будь який стан state = finally
+
+// console.log(promise);
+// promise.then((users) => {
+//   console.log("Ми в then", users);
+//   alert("Ви успішно відправили форму!");
+// });
+
+// promise.catch((error) => {
+//   console.error("Ми в catch", error.message);
+//   alert(error.message);
+// });
+
+// promise.finally(() => {
+//   isFormLoading = false;
+//   console.log("Ми в finally");
+// });
+
+// function handleSubmit(data) {
+//   isFormLoading = true;
+//   //....
 // }
 
-// function two() {
-//   let sum = 0;
-//   for (let i = 0; i < 1000000000; i++) {
-//     sum += i;
-//   }
-
-//   return sum;
+// function renderUsers(users) {
+//   const userList = document.querySelector(".list");
+//   users.forEach((user) => {
+//     const userLi = "...";
+//     userList.append(userLi);
+//   });
 // }
 
-// console.time();
-// console.log("before call");
-// // one();
-// setTimeout(one);
-// console.log("between call");
-// console.log("after call");
-// console.timeEnd();
+// GET, POST, PATCH, PUT & DELETE
+// PUT & PATCH = UPDATE
 
-// function outer() {
-//   console.log("outer");
+// function getUsers() {
+//   fetch("https://jsonplaceholder.typicode.com/users")
+//     .then((response) => {
+//       console.log(response);
 
-//   function inner() {
-//     console.log("inner");
-//   }
+//       if (response.ok) {
+//         const users = response.json();
+//         console.log(users);
+//         return users;
+//       }
 
-//   inner();
+//       throw new Error("щось сталось");
+//     })
+//     .then((users) => {
+//       console.log(users);
+//       return users;
+//     })
+//     .catch((error) => {
+//       console.log(error);
+//     })
+//     .finally((data) => {
+//       console.log("Все завершилось", data);
+//     });
 // }
 
-// outer();
+// getUsers();
 
-// []
-// ["outer"]
-// ["outer", "inner"]
-// ["outer", "inner", "log"]
-// ["outer", "inner"]
-// ["outer"]
-// []
-
-// function main() {
-//   setTimeout(function greet() {
-//     console.log("Hello");
-//   }, 2000);
-
-//   console.log("Bye!");
+// function getSomeData() {
+//   return new Promise(() => {});
 // }
+// console.log(getSomeData());
 
-// main();
+// const result = getSomeData()
+// result.then().catch()
 
-const result = fetch("http://localhost:3000/api/users"); // '[{id: 1, name: '}]'
-result.json(); // [{id: 1, name: '}]
+const form = document.querySelector("form");
 
-// /api/setUser
-// /api/getUsers
+function handleSubmit(event) {
+  event.preventDefault();
 
-// GET /api/users - отримання масиву користувачів
-// POST /api/users
-// PUT/PATCH (UPDATE) /api/users/1
-// DELETE /api/users/1
+  const userName = event.target.username.value;
+  const email = event.target.email.value;
+  const password = event.target.password.value;
 
-setTimeout(() => {
-  console.log("setTimeout");
-});
+  const user = {
+    userName: userName,
+    email,
+    password,
+  };
 
-const button = document.querySelector("button");
-button.addEventListener("click", () => {
-  console.log("click");
-  setTimeout(() => {
-    console.log("setTimeout click");
-  });
-});
+  console.log(user);
+  console.log(JSON.stringify(user));
+
+  fetch("https://jsonplaceholder.typicode.com/users", {
+    method: "POST",
+    body: JSON.stringify(user),
+  })
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      }
+
+      throw new Error(
+        "Не вдалось зреєструвати користувача, спробуйте пізніше!"
+      );
+    })
+    .then((data) => {
+      console.log(data);
+    })
+    .catch((error) => {
+      alert(error.message);
+    });
+}
+form.addEventListener("submit", handleSubmit);
+
+[1, 2, 3, 4, 5]
+  .map(() => {
+    //..
+  })
+  .filter(() => {})
+  .join();
