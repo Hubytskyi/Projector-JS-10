d00d4e51e360018b415bb24c2e2e634e4dff6603
Mykola Hubytskyi <hubytskyi@gmail.com>
2023-12-21 21:58:20 +0200

    add async 3

--- app.js
+++ app.js
@@ -1,137 +1,83 @@
 "use strict";
 
-// let isFormLoading = false;
-
-// const promise = new Promise((resolveFn, rejectFn) => {
-//   // ...
-
-//   const result = {
-//     data: [{ userId: 1, name: "John", age: 42 }],
-//     ok: true,
-//   };
-
-//   if (result.ok) {
-//     resolveFn(result.data);
-//     return;
-//   }
-
-//   rejectFn(new Error("щось трапилось"));
+// const p1 = Promise.resolve(3);
+// const p2 = 123;
+// const p3 = new Promise(function (resolve, reject) {
+//   setTimeout(() => {
+//     reject("some text");
+//   }, 2000);
 // });
 
-// promise стан = fulfilled --> then (приймає те що ми передали в resolveFn)
-// promise стан = rejected --> catch (приймає помилку яку ми передали в rejectFn)
-// promise будь який стан state = finally
-
-// console.log(promise);
-// promise.then((users) => {
-//   console.log("Ми в then", users);
-//   alert("Ви успішно відправили форму!");
+// const p4 = new Promise(function (resolve, reject) {
+//   setTimeout(() => {
+//     resolve("some text 2");
+//   }, 3000);
 // });
 
-// promise.catch((error) => {
-//   console.error("Ми в catch", error.message);
-//   alert(error.message);
+// Promise.any([p3, p4]).then((values) => {
+//   console.log(values);
 // });
 
-// promise.finally(() => {
-//   isFormLoading = false;
-//   console.log("Ми в finally");
-// });
-
-// function handleSubmit(data) {
-//   isFormLoading = true;
-//   //....
-// }
-
-// function renderUsers(users) {
-//   const userList = document.querySelector(".list");
-//   users.forEach((user) => {
-//     const userLi = "...";
-//     userList.append(userLi);
-//   });
-// }
-
-// GET, POST, PATCH, PUT & DELETE
-// PUT & PATCH = UPDATE
-
-// function getUsers() {
-//   fetch("https://jsonplaceholder.typicode.com/users")
+// function getUsers2() {
+//   let users = fetch("https://jsonplaceholder.typicode.com/users")
 //     .then((response) => {
-//       console.log(response);
-
 //       if (response.ok) {
-//         const users = response.json();
-//         console.log(users);
-//         return users;
+//         return response.json();
 //       }
-
-//       throw new Error("щось сталось");
 //     })
 //     .then((users) => {
-//       console.log(users);
 //       return users;
-//     })
-//     .catch((error) => {
-//       console.log(error);
-//     })
-//     .finally((data) => {
-//       console.log("Все завершилось", data);
 //     });
+//   console.log(users);
 // }
 
-// getUsers();
+// new Promise((resolve, reject) => {
+//   console.log("start");
 
-// function getSomeData() {
-//   return new Promise(() => {});
-// }
-// console.log(getSomeData());
+//   fetch("https://jsonplaceholder.typicode.com/users")
+//     .then((response) => {
+//       if (response.ok) {
+//         return response.json();
+//       }
+//     })
+//     .then((users) => {
+//       fetch("https://jsonplaceholder.typicode.com/users").then(
+//         (response) => {}
+//       );
+//     });
+// });
 
-// const result = getSomeData()
-// result.then().catch()
+let isLoading = false;
 
-const form = document.querySelector("form");
+async function getUsers() {
+  isLoading = true;
 
-function handleSubmit(event) {
-  event.preventDefault();
+  try {
+    const response = await fetch("https://jsonplaceholder.typicode.com/users");
 
-  const userName = event.target.username.value;
-  const email = event.target.email.value;
-  const password = event.target.password.value;
-
-  const user = {
-    userName: userName,
-    email,
-    password,
-  };
-
-  console.log(user);
-  console.log(JSON.stringify(user));
-
-  fetch("https://jsonplaceholder.typicode.com/users", {
-    method: "POST",
-    body: JSON.stringify(user),
-  })
-    .then((response) => {
-      if (response.ok) {
-        return response.json();
-      }
-
-      throw new Error(
-        "Не вдалось зреєструвати користувача, спробуйте пізніше!"
+    if (response.ok) {
+      const users = await response.json();
+      const response2 = await fetch(
+        "https://jsonplaceholder.typicode.com/todos"
       );
-    })
-    .then((data) => {
-      console.log(data);
-    })
-    .catch((error) => {
-      alert(error.message);
-    });
-}
-form.addEventListener("submit", handleSubmit);
+      const todos = await response2.json();
+      console.log(todos);
+      return;
+    }
 
-[1, 2, 3, 4, 5]
-  .map(() => {
-    //..
-  })
-  .filter(() => {})
-  .join();
+    // throw new Error("some error");
+  } catch (e) {
+    // (e)
+    console.log();
+  } finally {
+    isLoading = false;
+  }
+}
+
+getUsers();
+
+// try -> catch(e) -> finnaly
+// then(res) -> catch(e) -> finnaly
+
+// const button = doc...
+// button.addEventListener
